2683fe838e544cda17e04e25e40b5fe29ab2bc77
RubemJanoni <>
2023-08-24 22:11:47 +0000

    add signup view

--- task/views.py
+++ task/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 # home project
@@ -9,4 +12,37 @@ def home(request):
 
 
 def signup(request):
-    return render(request, 'signup.html')
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+
+            'form': UserCreationForm,
+
+        })
+
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+
+                login(request, user)
+                return redirect('tasks')
+
+            except:
+
+                return render(request, 'signup.html', {
+
+                    'form': UserCreationForm,
+                    "error": 'User already exist'
+
+                })
+
+        return render(request, 'signup.html', {
+
+            'form': UserCreationForm,
+            "error": 'Password different'
+
+        })
