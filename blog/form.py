1fed815e75f1d86ad391904eb7b44181406e0fa9
RubemJanoni <>
2023-12-13 14:01:50 +0000

    Adjust on publication_form

--- blog/form.py
+++ blog/form.py
@@ -2,10 +2,13 @@ from django import forms
 from blog.models import Post
 
 
-class Publication_form(forms.ModelForm):
-    
+class Publication_form(forms.ModelForm):    
 
     class Meta:
         model = Post
         # fields = '__all__'
         fields = ['title', 'author', 'featured_image', 'content', 'status']
+
+    featured_image = forms.ImageField(label='Upload Image')
+
+
